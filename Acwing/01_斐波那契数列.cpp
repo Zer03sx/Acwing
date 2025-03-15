b989a57d89cba37ff253af2acc50b357759bc3b9
Zer03sx <zer03116745006@gmail.com>
2025-03-15 21:00:32 +0800

    完成斐波那契数列

---
+++ Acwing/01_斐波那契数列.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int func(int n)
+{
+	if (n == 1)
+		return 1;
+	if (n == 2)
+		return 2;
+	return func(n - 1) + func(n - 2);
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << func(n) << endl;
+
+	system("pause");
+	return 0;
+}
